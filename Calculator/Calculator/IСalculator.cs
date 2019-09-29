240604ef2fdb536c8d4bcc03a08780708f34c695
Roman Samoylov <samoylov.rim2002@gmail.com>
2019-09-29 20:02:34 +0300

    Start code of calculating operations

---
+++ Calculator/Calculator/IСalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface IСalculator
+    {
+        Task<double> Addition(int a, int b);
+
+        Task<double> Substraction(int a, int b);
+
+        Task<double> Multiplication(int a, int b);
+
+        Task<double> Divison(int a, int b);
+
+        Task<double> Result(string operations);
+    }
+}
