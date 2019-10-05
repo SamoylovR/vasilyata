0369ae75021264d56eda16f3c9e5c51e408eefbc
AnastasiaKorotkevich <korotkevich_a_a@mail.ru>
2019-10-06 01:23:49 +0300

    princess (not final version)

---
+++ princess/ConsoleApp1/Console.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class GameConsoleString
+	{
+		public ConsoleColor color { get; set; } = ConsoleColor.White;
+		public string msg { get; set; }
+		public GameConsoleString(string msg, ConsoleColor color)
+		{
+			this.color = color;
+			this.msg = msg;
+		}
+
+	}
+    class GameConsole
+    {
+        List<GameConsoleString> log;
+        int rows = 0;
+        int col = 0;
+
+        public GameConsole(int col)
+        {
+            log = new List<GameConsoleString>();
+            this.col = col;
+            rows = 20;
+        }
+
+        public void Render()
+        {
+			for (int i = 0; i < log.Count; i++)
+			{
+				//Console.ForegroundColor = log[log.Count - i].color;
+				Console.SetCursorPosition(col, rows - i);
+				//Console.WriteLine(log[log.Count - i].msg.PadRight(Console.BufferWidth - col), ' ');
+			}
+		}
+
+        public void addMessage(GameConsoleString str)
+        {
+            log.Add(str);
+
+            while(log.Count > rows)
+                log.RemoveAt(0);
+
+			Render();
+		}
+    }
+}
---
+++ princess/ConsoleApp1/Properties.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Properties
+    {
+        private static Dictionary<string, Object> map = new Dictionary<string, Object>()
+        {
+            // id key               char name   HP  AR   AC   DX  AP  AMBUSH
+            { "hero_tank",  new Object("tank", 150, 20,  40,  30,  75, false)},
+            { "hero_norm",  new Object("norm", 100, 10,  60,  50,  50, false)},
+            { "hero_lucky", new Object("luck",  50,  5,  80,  70,  30, false)},
+            { "trap_jun",   new Object("jun",   10,  0, 100,  20,  30, true)},
+            { "trap_mid",   new Object("mid",   30,  2, 100,  30,  30, true)},
+            { "trap_sen",   new Object("sen",   50,  5, 100,  30,  30, true)},
+            { "princess",   new Object("princ", 25,  0,  10, 100, 100, false)},
+        };
+                
+        public static Object Get(string key)
+        {
+            if (map.Count == 0)
+                return null;
+
+            return map.ContainsKey(key) ? map[key] : null;
+        }
+    }
+}
