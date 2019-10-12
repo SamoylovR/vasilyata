9be3a6d2fdcddc156b2ecf8f0bfdaa98d1e9c31e
Egor <egor.pritula02@mail.ru>
2019-10-13 00:53:46 +0300

    Сделал систему регистрации и логина, пока что только при входе, Рома чекни)))

---
+++ Bank/Bank/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logining
+{
+    class Registration
+    {
+        public static (string, string) registration(string UserLogin, string UserPassword)
+        {
+            var RegistrationField = (UserLogin, UserPassword);
+            Console.Clear();
+            Console.WriteLine("Регистрация прошла успешно, введите логин и пароль заново");
+            return RegistrationField;
+        }
+    }
+    class Login
+    {
+       public static int login(string RealLogin, string RealPassword, string WritedLogin, string WritedPassword)
+        {
+            RealLogin = RealLogin.Replace(" ", "");
+            WritedLogin = WritedLogin.Replace(" ","");
+            RealPassword = RealPassword.Replace(" ","");
+            WritedPassword = WritedPassword.Replace(" ","");
+            if (RealLogin == WritedLogin && RealPassword == WritedPassword)
+                return 1;
+            else return 0;
+            }
+        } 
+    }
+
--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -11,6 +11,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите логин и пароль для регистрации");
+           var (Login,Password)= Logining.Registration.registration(Console.ReadLine(), Console.ReadLine()) ;
+            while (true)
+            {
+                if (Logining.Login.login(Login, Password, Console.ReadLine(), Console.ReadLine()) == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введенные вами данные были одобрены, вход в систему...\n");
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Неверные логин или пароль");
+                }
+            }
             double money = 0;
             while (true)
             {
